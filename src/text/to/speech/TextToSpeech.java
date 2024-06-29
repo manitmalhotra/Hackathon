f78d1a72dd66cef1247aecb07915ee3e80a17d39
Manit Malhotra <manitmalhotra0909@gmail.com>
2024-06-29 12:12:47 +0530

    Initial Commit

---
+++ src/text/to/speech/TextToSpeech.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package text.to.speech;
+
+/**
+ *
+ * @author manit
+ */
+
+public class TextToSpeech {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Start n=new Start();
+        n.show();
+        
+        
+    }
+    
+}
